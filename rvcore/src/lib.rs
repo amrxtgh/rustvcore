074c259b20d580251a505d4de6b792dd3a2cbc4d
amrxtgh <ghimireamrit619@gmail.com>
2026-02-23 03:35:11 +0545

    feat: implemented the CPU struct and add my first instruction add

--- rvcore/src/lib.rs
+++ rvcore/src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct CPU {
+    pub regs: [u32; 32], //x0-x31
+    pub pc: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl CPU {
+    pub fn new() -> Self {
+        Self {
+            regs: [0; 32],
+            pc: 0,
+        }
+    }
+    pub fn read_reg(&self, index: usize) -> u32 {
+        if index == 0 { 0 } else { self.regs[index] } // ensure rv x0 is always 0
+    }
+    pub fn write_reg(&mut self, index: usize, value: u32) {
+        if index != 0 { let _ = self.regs[index] = value; } // ensure writing to x0 not possible
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn add(&mut self, rd: usize, rs1: usize, rs2: usize) {
+        let val = self.read_reg(rs1).wrapping_add(self.read_reg(rs2));
+        self.write_reg(rd, val);
     }
 }
