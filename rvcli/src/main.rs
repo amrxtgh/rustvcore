302c9ebee91906c66c500b61ce2c2b2ef9005899
amrxtgh <ghimireamrit619@gmail.com>
2026-02-23 03:34:34 +0545

    feat: add the main feature in the risc-v cli normal test

--- rvcli/src/main.rs
+++ rvcli/src/main.rs
@@ -1,3 +1,13 @@
+use rvcore::CPU;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU::new();
+    cpu.regs[1] = 10;
+    cpu.regs[2] = 20;
+    
+    cpu.add(3, 1, 2);
+    println!("x3 = {}", cpu.read_reg(3));
+    //cpu.write_reg(1, 42);
+    //println!("x1 = {}", cpu.read_reg(1));
+    //println!("x0 = {}", cpu.read_reg(0)); // always 0
 }
