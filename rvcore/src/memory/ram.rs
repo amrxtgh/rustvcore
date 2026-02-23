066a51c8991452b77772d918cf79e872a1c05783
amrxtgh <ghimireamrit619@gmail.com>
2026-02-24 01:53:51 +0545

    added: RAM blueprint

---
+++ rvcore/src/memory/ram.rs
@@ -0,0 +1,18 @@
+pub struct RAM {
+    pub data: Vec<u8>,
+}
+
+impl RAM {
+    pub fn new(size: usize) -> Self {
+        Self {
+            data: vec![0, size],
+        }
+    }
+    pub fn load32(&self, addr: usize) {
+
+    }
+
+    pub fn store32(&self, addr: usize, val: u32) {
+
+    }
+}
