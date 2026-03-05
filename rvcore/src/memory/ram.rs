cdccb9d8203959d109469bf2491e3db09fdb5d50
amrxtgh <ghimireamrit619@gmail.com>
2026-03-05 09:26:13 +0545

    add: simple change

--- rvcore/src/memory/ram.rs
+++ rvcore/src/memory/ram.rs
@@ -1,14 +1,19 @@
 pub struct RAM {
     pub data: Vec<u8>,
 }
-
 impl RAM {
     pub fn new(size: usize) -> Self {
         Self {
             data: vec![0; size],
         }
     }
-    pub fn load32(&self, addr: usize) {}
+    pub fn load32(&self, addr: usize) {
 
-    pub fn store32(&self, addr: usize, val: u32) {}
+    }
+
+    pub fn store32(&self, addr: usize, val: u32) {
+    }
 }
+
+
+
