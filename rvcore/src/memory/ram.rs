dfccb39a212be3dbf90cef1c3dc997c092e09897
amrxtgh <ghimireamrit619@gmail.com>
2026-03-05 22:17:11 +0545

    refactor: remove size parameter from RAM constructor and use default DRAM_SIZE

--- rvcore/src/memory/ram.rs
+++ rvcore/src/memory/ram.rs
@@ -1,19 +1,14 @@
+/// Default DRAM size: 1 GiB.
+pub const DRAM_SIZE: u64 = 1024 * 1024 * 1024;
 pub struct RAM {
     pub data: Vec<u8>,
 }
 impl RAM {
-    pub fn new(size: usize) -> Self {
+    pub fn new() -> Self {
         Self {
-            data: vec![0; size],
+            data: vec![0; DRAM_SIZE as usize],
         }
     }
-    pub fn load32(&self, addr: usize) {
-
-    }
-
-    pub fn store32(&self, addr: usize, val: u32) {
-    }
+    pub fn load32(&self, _addr: u32) {}
+    pub fn store32(&self, _addr: u32, _val: u32) {}
 }
-
-
-
